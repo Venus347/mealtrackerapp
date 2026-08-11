7211fa24aee1a9e8971d408c30c5462c6bbd573d
Nusrat Jerin <nusratjerin401@gmail.com>
2026-08-11 13:12:09 -0400

    fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
+import mealRoutes from "./routes/meals.js";
 
 const app = express();
-
 app.use(express.json());
 
+
+//GET
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Diet API is running!");
 });
 
+app.use("/api", mealRoutes);
 const PORT = 3000;
 
 app.listen(PORT, () => {
